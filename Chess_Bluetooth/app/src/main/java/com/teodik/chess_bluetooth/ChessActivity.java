37f72b4757e29fdd9f356ff7a241d327b8b52a7e
teodik <teodik1979@gmail.com>
2017-08-14 22:17:21 -0700

    chess layout is done

---
+++ Chess_Bluetooth/app/src/main/java/com/teodik/chess_bluetooth/ChessActivity.java
@@ -0,0 +1,375 @@
+package com.teodik.chess_bluetooth;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class ChessActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button b1;
+    private Button b2;
+    private Button b3;
+    private Button b4;
+    private Button b5;
+    private Button b6;
+    private Button b7;
+    private Button b8;
+    private Button b9;
+    private Button b10;
+    private Button b11;
+    private Button b12;
+    private Button b13;
+    private Button b14;
+    private Button b15;
+    private Button b16;
+    private Button b17;
+    private Button b18;
+    private Button b19;
+    private Button b20;
+    private Button b21;
+    private Button b22;
+    private Button b23;
+    private Button b24;
+    private Button b25;
+    private Button b26;
+    private Button b27;
+    private Button b28;
+    private Button b29;
+    private Button b30;
+    private Button b31;
+    private Button b32;
+    private Button b33;
+    private Button b34;
+    private Button b35;
+    private Button b36;
+    private Button b37;
+    private Button b38;
+    private Button b39;
+    private Button b40;
+    private Button b41;
+    private Button b42;
+    private Button b43;
+    private Button b44;
+    private Button b45;
+    private Button b46;
+    private Button b47;
+    private Button b48;
+    private Button b49;
+    private Button b50;
+    private Button b51;
+    private Button b52;
+    private Button b53;
+    private Button b54;
+    private Button b55;
+    private Button b56;
+    private Button b57;
+    private Button b58;
+    private Button b59;
+    private Button b60;
+    private Button b61;
+    private Button b62;
+    private Button b63;
+    private Button b64;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chess);
+        b1 = (Button) findViewById(R.id.b1);
+        b2 = (Button) findViewById(R.id.b2);
+        b3 = (Button) findViewById(R.id.b3);
+        b4 = (Button) findViewById(R.id.b4);
+        b5 = (Button) findViewById(R.id.b5);
+        b6 = (Button) findViewById(R.id.b6);
+        b7 = (Button) findViewById(R.id.b7);
+        b8 = (Button) findViewById(R.id.b8);
+        b9 = (Button) findViewById(R.id.b9);
+        b10 = (Button) findViewById(R.id.b10);
+        b11 = (Button) findViewById(R.id.b11);
+        b12 = (Button) findViewById(R.id.b12);
+        b13 = (Button) findViewById(R.id.b13);
+        b14 = (Button) findViewById(R.id.b14);
+        b15 = (Button) findViewById(R.id.b15);
+        b16 = (Button) findViewById(R.id.b16);
+        b17 = (Button) findViewById(R.id.b17);
+        b18 = (Button) findViewById(R.id.b18);
+        b19 = (Button) findViewById(R.id.b19);
+        b20 = (Button) findViewById(R.id.b20);
+        b21 = (Button) findViewById(R.id.b21);
+        b22 = (Button) findViewById(R.id.b22);
+        b23 = (Button) findViewById(R.id.b23);
+        b24 = (Button) findViewById(R.id.b24);
+        b25 = (Button) findViewById(R.id.b25);
+        b26 = (Button) findViewById(R.id.b26);
+        b27 = (Button) findViewById(R.id.b27);
+        b28 = (Button) findViewById(R.id.b28);
+        b29 = (Button) findViewById(R.id.b29);
+        b30 = (Button) findViewById(R.id.b30);
+        b31 = (Button) findViewById(R.id.b31);
+        b32 = (Button) findViewById(R.id.b32);
+        b33 = (Button) findViewById(R.id.b33);
+        b34 = (Button) findViewById(R.id.b34);
+        b35 = (Button) findViewById(R.id.b35);
+        b36 = (Button) findViewById(R.id.b36);
+        b37 = (Button) findViewById(R.id.b37);
+        b38 = (Button) findViewById(R.id.b38);
+        b39 = (Button) findViewById(R.id.b39);
+        b40 = (Button) findViewById(R.id.b40);
+        b41 = (Button) findViewById(R.id.b41);
+        b42 = (Button) findViewById(R.id.b42);
+        b43 = (Button) findViewById(R.id.b43);
+        b44 = (Button) findViewById(R.id.b44);
+        b45 = (Button) findViewById(R.id.b45);
+        b46 = (Button) findViewById(R.id.b46);
+        b47 = (Button) findViewById(R.id.b47);
+        b48 = (Button) findViewById(R.id.b48);
+        b49 = (Button) findViewById(R.id.b49);
+        b50 = (Button) findViewById(R.id.b50);
+        b51 = (Button) findViewById(R.id.b51);
+        b52 = (Button) findViewById(R.id.b52);
+        b53 = (Button) findViewById(R.id.b53);
+        b54 = (Button) findViewById(R.id.b54);
+        b55 = (Button) findViewById(R.id.b55);
+        b56 = (Button) findViewById(R.id.b56);
+        b57 = (Button) findViewById(R.id.b57);
+        b58 = (Button) findViewById(R.id.b58);
+        b59 = (Button) findViewById(R.id.b59);
+        b60 = (Button) findViewById(R.id.b60);
+        b61 = (Button) findViewById(R.id.b61);
+        b62 = (Button) findViewById(R.id.b62);
+        b63 = (Button) findViewById(R.id.b63);
+        b64 = (Button) findViewById(R.id.b64);
+        b1.setText("BR");
+        b2.setText("BN");
+        b3.setText("BB");
+        b4.setText("BK");
+        b5.setText("BQ");
+        b6.setText("BB");
+        b7.setText("BN");
+        b8.setText("BR");
+        b9.setText("BP");
+        b10.setText("BP");
+        b11.setText("BP");
+        b12.setText("BP");
+        b13.setText("BP");
+        b14.setText("BP");
+        b15.setText("BP");
+        b16.setText("BP");
+        b49.setText("WP");
+        b50.setText("WP");
+        b51.setText("WP");
+        b52.setText("WP");
+        b53.setText("WP");
+        b54.setText("WP");
+        b55.setText("WP");
+        b56.setText("WP");
+        b57.setText("WR");
+        b58.setText("WN");
+        b59.setText("WB");
+        b60.setText("WK");
+        b61.setText("WQ");
+        b62.setText("WB");
+        b63.setText("WN");
+        b64.setText("WR");
+
+
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
+        b5.setOnClickListener(this);
+        b6.setOnClickListener(this);
+        b7.setOnClickListener(this);
+        b8.setOnClickListener(this);
+        b9.setOnClickListener(this);
+        b10.setOnClickListener(this);
+        b11.setOnClickListener(this);
+        b12.setOnClickListener(this);
+        b13.setOnClickListener(this);
+        b14.setOnClickListener(this);
+        b15.setOnClickListener(this);
+        b16.setOnClickListener(this);
+        b17.setOnClickListener(this);
+        b18.setOnClickListener(this);
+        b19.setOnClickListener(this);
+        b20.setOnClickListener(this);
+        b21.setOnClickListener(this);
+        b22.setOnClickListener(this);
+        b23.setOnClickListener(this);
+        b24.setOnClickListener(this);
+        b25.setOnClickListener(this);
+        b26.setOnClickListener(this);
+        b27.setOnClickListener(this);
+        b28.setOnClickListener(this);
+        b29.setOnClickListener(this);
+        b30.setOnClickListener(this);
+        b31.setOnClickListener(this);
+        b32.setOnClickListener(this);
+        b33.setOnClickListener(this);
+        b34.setOnClickListener(this);
+        b35.setOnClickListener(this);
+        b36.setOnClickListener(this);
+        b37.setOnClickListener(this);
+        b38.setOnClickListener(this);
+        b39.setOnClickListener(this);
+        b40.setOnClickListener(this);
+        b41.setOnClickListener(this);
+        b42.setOnClickListener(this);
+        b43.setOnClickListener(this);
+        b44.setOnClickListener(this);
+        b45.setOnClickListener(this);
+        b46.setOnClickListener(this);
+        b47.setOnClickListener(this);
+        b48.setOnClickListener(this);
+        b49.setOnClickListener(this);
+        b50.setOnClickListener(this);
+        b51.setOnClickListener(this);
+        b52.setOnClickListener(this);
+        b53.setOnClickListener(this);
+        b54.setOnClickListener(this);
+        b55.setOnClickListener(this);
+        b56.setOnClickListener(this);
+        b57.setOnClickListener(this);
+        b58.setOnClickListener(this);
+        b59.setOnClickListener(this);
+        b60.setOnClickListener(this);
+        b61.setOnClickListener(this);
+        b62.setOnClickListener(this);
+        b63.setOnClickListener(this);
+        b64.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.b1:
+                break;
+            case R.id.b2:
+                break;
+            case R.id.b3:
+                break;
+            case R.id.b4:
+                break;
+            case R.id.b5:
+                break;
+            case R.id.b6:
+                break;
+            case R.id.b7:
+                break;
+            case R.id.b8:
+                break;
+            case R.id.b9:
+                break;
+            case R.id.b10:
+                break;
+            case R.id.b11:
+                break;
+            case R.id.b12:
+                break;
+            case R.id.b13:
+                break;
+            case R.id.b14:
+                break;
+            case R.id.b15:
+                break;
+            case R.id.b16:
+                break;
+            case R.id.b17:
+                break;
+            case R.id.b18:
+                break;
+            case R.id.b19:
+                break;
+            case R.id.b20:
+                break;
+            case R.id.b21:
+                break;
+            case R.id.b22:
+                break;
+            case R.id.b23:
+                break;
+            case R.id.b24:
+                break;
+            case R.id.b25:
+                break;
+            case R.id.b26:
+                break;
+            case R.id.b27:
+                break;
+            case R.id.b28:
+                break;
+            case R.id.b29:
+                break;
+            case R.id.b30:
+                break;
+            case R.id.b31:
+                break;
+            case R.id.b32:
+                break;
+            case R.id.b33:
+                break;
+            case R.id.b34:
+                break;
+            case R.id.b35:
+                break;
+            case R.id.b36:
+                break;
+            case R.id.b37:
+                break;
+            case R.id.b38:
+                break;
+            case R.id.b39:
+                break;
+            case R.id.b40:
+                break;
+            case R.id.b41:
+                break;
+            case R.id.b42:
+                break;
+            case R.id.b43:
+                break;
+            case R.id.b44:
+                break;
+            case R.id.b45:
+                break;
+            case R.id.b46:
+                break;
+            case R.id.b47:
+                break;
+            case R.id.b48:
+                break;
+            case R.id.b49:
+                break;
+            case R.id.b50:
+                break;
+            case R.id.b51:
+                break;
+            case R.id.b52:
+                break;
+            case R.id.b53:
+                break;
+            case R.id.b54:
+                break;
+            case R.id.b55:
+                break;
+            case R.id.b56:
+                break;
+            case R.id.b57:
+                break;
+            case R.id.b58:
+                break;
+            case R.id.b59:
+                break;
+            case R.id.b60:
+                break;
+            case R.id.b61:
+                break;
+            case R.id.b62:
+                break;
+            case R.id.b63:
+                break;
+            case R.id.b64:
+                break;
+        }
+    }
+}
---
+++ Chess_Bluetooth/app/src/main/java/com/teodik/chess_bluetooth/MainActivity.java
@@ -0,0 +1,16 @@
+package com.teodik.chess_bluetooth;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, ChessActivity.class);
+        startActivity(intent);
+    }
+}
