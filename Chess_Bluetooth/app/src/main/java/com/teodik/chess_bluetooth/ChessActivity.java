c0a224ceac509f46a51614fa95ccc2e4e35f8ed4
teodik <teodik1979@gmail.com>
2017-08-19 14:17:53 -0700

    Button array fixed

--- Chess_Bluetooth/app/src/main/java/com/teodik/chess_bluetooth/ChessActivity.java
+++ Chess_Bluetooth/app/src/main/java/com/teodik/chess_bluetooth/ChessActivity.java
@@ -4,9 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class ChessActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private String piece;
+    private int location;
+    private boolean turn;
+    private Button b0;
     private Button b1;
     private Button b2;
     private Button b3;
@@ -70,11 +75,16 @@ public class ChessActivity extends AppCompatActivity implements View.OnClickList
     private Button b61;
     private Button b62;
     private Button b63;
-    private Button b64;
+
+    private Button[] buttons;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chess);
+        turn = true;
+        piece = "";
+        location = -1;
+        b0 = (Button) findViewById(R.id.b0);
         b1 = (Button) findViewById(R.id.b1);
         b2 = (Button) findViewById(R.id.b2);
         b3 = (Button) findViewById(R.id.b3);
@@ -138,15 +148,15 @@ public class ChessActivity extends AppCompatActivity implements View.OnClickList
         b61 = (Button) findViewById(R.id.b61);
         b62 = (Button) findViewById(R.id.b62);
         b63 = (Button) findViewById(R.id.b63);
-        b64 = (Button) findViewById(R.id.b64);
-        b1.setText("BR");
-        b2.setText("BN");
-        b3.setText("BB");
-        b4.setText("BK");
-        b5.setText("BQ");
-        b6.setText("BB");
-        b7.setText("BN");
-        b8.setText("BR");
+        b0.setText("BR");
+        b1.setText("BN");
+        b2.setText("BB");
+        b3.setText("BK");
+        b4.setText("BQ");
+        b5.setText("BB");
+        b6.setText("BN");
+        b7.setText("BR");
+        b8.setText("BP");
         b9.setText("BP");
         b10.setText("BP");
         b11.setText("BP");
@@ -154,7 +164,7 @@ public class ChessActivity extends AppCompatActivity implements View.OnClickList
         b13.setText("BP");
         b14.setText("BP");
         b15.setText("BP");
-        b16.setText("BP");
+        b48.setText("WP");
         b49.setText("WP");
         b50.setText("WP");
         b51.setText("WP");
@@ -162,17 +172,15 @@ public class ChessActivity extends AppCompatActivity implements View.OnClickList
         b53.setText("WP");
         b54.setText("WP");
         b55.setText("WP");
-        b56.setText("WP");
-        b57.setText("WR");
-        b58.setText("WN");
-        b59.setText("WB");
-        b60.setText("WK");
-        b61.setText("WQ");
-        b62.setText("WB");
-        b63.setText("WN");
-        b64.setText("WR");
-
-
+        b56.setText("WR");
+        b57.setText("WN");
+        b58.setText("WB");
+        b59.setText("WK");
+        b60.setText("WQ");
+        b61.setText("WB");
+        b62.setText("WN");
+        b63.setText("WR");
+        b0.setOnClickListener(this);
         b1.setOnClickListener(this);
         b2.setOnClickListener(this);
         b3.setOnClickListener(this);
@@ -236,12 +244,100 @@ public class ChessActivity extends AppCompatActivity implements View.OnClickList
         b61.setOnClickListener(this);
         b62.setOnClickListener(this);
         b63.setOnClickListener(this);
-        b64.setOnClickListener(this);
+        buttons = new Button[64];
+        buttons[0] = b0;
+        buttons[1] = b1;
+        buttons[2] = b2;
+        buttons[3] = b3;
+        buttons[4] = b4;
+        buttons[5] = b5;
+        buttons[6] = b6;
+        buttons[7] = b7;
+        buttons[8] = b8;
+        buttons[9] = b9;
+        buttons[10] = b10;
+        buttons[11] = b11;
+        buttons[12] = b12;
+        buttons[13] = b13;
+        buttons[14] = b14;
+        buttons[15] = b15;
+        buttons[16] = b16;
+        buttons[17] = b17;
+        buttons[18] = b18;
+        buttons[19] = b19;
+        buttons[20] = b20;
+        buttons[21] = b21;
+        buttons[22] = b22;
+        buttons[23] = b23;
+        buttons[24] = b24;
+        buttons[25] = b25;
+        buttons[26] = b26;
+        buttons[27] = b27;
+        buttons[28] = b28;
+        buttons[29] = b29;
+        buttons[30] = b30;
+        buttons[31] = b31;
+        buttons[32] = b32;
+        buttons[33] = b33;
+        buttons[34] = b34;
+        buttons[35] = b35;
+        buttons[36] = b36;
+        buttons[37] = b37;
+        buttons[38] = b38;
+        buttons[39] = b39;
+        buttons[40] = b40;
+        buttons[41] = b41;
+        buttons[42] = b42;
+        buttons[43] = b43;
+        buttons[44] = b44;
+        buttons[45] = b45;
+        buttons[46] = b46;
+        buttons[47] = b47;
+        buttons[48] = b48;
+        buttons[49] = b49;
+        buttons[50] = b50;
+        buttons[51] = b51;
+        buttons[52] = b52;
+        buttons[53] = b53;
+        buttons[54] = b54;
+        buttons[55] = b55;
+        buttons[56] = b56;
+        buttons[57] = b57;
+        buttons[58] = b58;
+        buttons[59] = b59;
+        buttons[60] = b60;
+        buttons[61] = b61;
+        buttons[62] = b62;
+        buttons[63] = b63;
     }
 
     @Override
     public void onClick(View v) {
         switch(v.getId()){
+            case R.id.b0:
+                if(piece.equals("")&&location == -1) {
+                    piece = b0.getText().toString();
+                    location = 0;
+                }
+                else if(piece.equals(b0.getText().toString())&& location == 0){
+                    piece = "";
+                    location = -1;
+                    Toast.makeText(ChessActivity.this, "Move Cancelled!", Toast.LENGTH_LONG).show();
+
+                }
+                else{
+                    if(equaluate(piece, location, 0)){
+                        turn = !turn;
+                        piece = "";
+                        location = -1;
+                    }
+                    else{
+                        piece = "";
+                        location = -1;
+                        Toast.makeText(ChessActivity.this, "Not a valid move!", Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
             case R.id.b1:
                 break;
             case R.id.b2:
@@ -259,6 +355,28 @@ public class ChessActivity extends AppCompatActivity implements View.OnClickList
             case R.id.b8:
                 break;
             case R.id.b9:
+                if(piece.equals("")&&location == -1) {
+                    piece = b9.getText().toString();
+                    location = 8;
+                }
+                else if(piece.equals(b9.getText().toString())&& location == 8){
+                    piece = "";
+                    location = -1;
+                    Toast.makeText(ChessActivity.this, "Move Cancelled!", Toast.LENGTH_LONG).show();
+
+                }
+                else{
+                    if(equaluate(piece, location, 8)){
+                        turn = !turn;
+                        piece = "";
+                        location = -1;
+                    }
+                    else{
+                        piece = "";
+                        location = -1;
+                        Toast.makeText(ChessActivity.this, "Not a valid move!", Toast.LENGTH_LONG).show();
+                    }
+                }
                 break;
             case R.id.b10:
                 break;
@@ -368,8 +486,67 @@ public class ChessActivity extends AppCompatActivity implements View.OnClickList
                 break;
             case R.id.b63:
                 break;
-            case R.id.b64:
-                break;
         }
+    }
+
+    private boolean equaluate(String piece, int location, int i) {
+        if(piece.charAt(1) == 'R' || piece.charAt(1) == 'P')
+            return rook(piece, location, i);
+        else if(piece.charAt(1) == 'B')
+            return false;
+        return false;
+    }
+
+    private boolean rook(String piece, int location, int i) {
+        int j = location + 8;
+        while(j >= 0 && j < 64){
+            if(j == i){
+                buttons[j].setText(piece);
+                buttons[location].setText("");
+                return true;
+            }
+            if(!buttons[j].getText().toString().equals(""))
+                break;
+            j += 8;
+        }
+        j = location - 8;
+        while(j >= 0 && j < 64){
+            if(j == i){
+                buttons[j].setText(piece);
+                buttons[location].setText("");
+                return true;
+            }
+            if(!buttons[j].getText().toString().equals(""))
+                break;
+            j -= 8;
+        }
+        j = location + 1;
+        while(j >= 0 && j < 64){
+            if(j == i){
+                buttons[j].setText(piece);
+                buttons[location].setText("");
+                return true;
+            }
+            if(!buttons[j].getText().toString().equals("")){
+                break;
+            }
+            if(j % 8 == 0 && (j + 1) % 8 == 1)
+                break;
+            j += 1;
+        }
+        j = location - 1;
+        while(j >= 0 && j < 64){
+            if(j == i){
+                buttons[j].setText(piece);
+                buttons[location].setText("");
+                return true;
+            }
+            if(!buttons[j].getText().toString().equals(""))
+                break;
+            if(j % 8 ==1 && (j - 1) % 8 == 0)
+                break;
+            j -= 1;
+        }
+        return false;
     }
 }
